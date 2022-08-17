b16207cb6f31723c4443e11d50e50ceb12968c24
Tommy Hanks <tommy.hanks@gmail.com>
2022-08-17 06:04:40 -0400

    Current progress wiring up the RedisClusterDatabase.

--- ReadUs/RedisClusterCommandsPool.cs
+++ ReadUs/RedisClusterCommandsPool.cs
@@ -1,20 +1,26 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using static ReadUs.Encoder.Encoder;
 using static ReadUs.RedisCommandNames;
 
 namespace ReadUs
 {
-    public class RedisClusterCommandsPool : RedisCommandsPool
+    public class RedisClusterCommandsPool : IDisposable
     {
+        private readonly ConcurrentQueue<RedisClusterConnection> _backingPool = new ConcurrentQueue<RedisClusterConnection>();
+        private readonly List<RedisClusterConnection> _allConnections = new List<RedisClusterConnection>();
+
         private ClusterNodesResult _existingClusterNodes;
 
-        public RedisClusterCommandsPool(string serverAddress, int serverPort) : 
-            base(serverAddress, serverPort) => ResolveClusterNodes();
+        public RedisClusterCommandsPool(string serverAddress, int serverPort) =>
+            ResolveClusterNodes(serverAddress, serverPort);
 
-        private void ResolveClusterNodes()
+        private void ResolveClusterNodes(string serverAddress, int serverPort)
         {
             // First, let's create a connection to whatever server that was provided.
-            var initialConnection = new RedisConnection(_serverAddress, _serverPort);
+            var initialConnection = new RedisConnection(serverAddress, serverPort);
             initialConnection.Connect();
 
             // Next, execute the `cluster nodes` command to get an inventory of the cluster.
@@ -29,6 +35,43 @@ namespace ReadUs
             // TODO: Think about how to make this more robust. This won't survive any kind of change
             //       to the cluster. 
             _existingClusterNodes = nodes;
+        }
+
+        public async Task<RedisClusterDatabase> GetAsync()
+        {
+            var connection = GetReadUsConnection();
+
+            if (!connection.IsConnected)
+            {
+                await connection.ConnectAsync();
+            }
+
+            return new RedisClusterDatabase(connection, this);
+        }
+
+        private RedisClusterConnection GetReadUsConnection()
+        {
+            if (_backingPool.TryDequeue(out var connection))
+            {
+                return connection;
+            }
+
+            var newConnection = new RedisClusterConnection(_existingClusterNodes);
+
+            _allConnections.Add(newConnection);
+
+            return newConnection;
+        }
+
+        internal void ReturnConnection(RedisClusterConnection connection) =>
+            _backingPool.Enqueue(connection);
+
+        public void Dispose()
+        {
+            foreach (var connection in _allConnections)
+            {
+                connection.Dispose();
+            }
         }
     }
 }
--- ReadUs/RedisClusterDatabase.cs
+++ ReadUs/RedisClusterDatabase.cs
@@ -1,7 +1,12 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using ReadUs.Exceptions;
+using static ReadUs.Encoder.Encoder;
+using static ReadUs.Parser.Parser;
+using static ReadUs.RedisCommandNames;
+using static ReadUs.ParameterUtilities;
+using ReadUs.Parser;
+using System.Runtime.CompilerServices;
 
 namespace ReadUs
 {
@@ -36,9 +41,19 @@ namespace ReadUs
             throw new NotImplementedException();
         }
 
-        public Task<string> GetAsync(string key)
+        public async Task<string> GetAsync(string key)
         {
-            throw new NotImplementedException();
+            CheckIfDisposed();
+
+            var rawCommand = Encode(Get, key);
+
+            var rawResult = await _connection.SendCommandAsync(key, rawCommand).ConfigureAwait(false);
+
+            var result = Parse(rawResult);
+
+            EvaluateResultAndThrow(result);
+
+            return result.ToString();
         }
 
         public Task<int> LeftPushAsync(string key, params string[] element)
@@ -58,7 +73,11 @@ namespace ReadUs
 
         public Task SelectAsync(int databaseId)
         {
-            throw new NotImplementedException();
+            CheckIfDisposed();
+
+            // No-Op, this command doesn't really do anything on clusters. 
+
+            return Task.CompletedTask;
         }
 
         public Task SetAsync(string key, string value)
@@ -66,9 +85,29 @@ namespace ReadUs
             throw new NotImplementedException();
         }
 
+        private bool _isDisposed = false;
+
         public void Dispose()
         {
-            throw new NotImplementedException();
+            _pool.ReturnConnection(_connection);
+
+        }
+
+        private void CheckIfDisposed()
+        {
+            if (_isDisposed)
+            {
+                throw new RedisDatabaseDisposedException("This instance of `RedisClusterDatabase` has already been disposed.");
+            }
+        }
+
+        // TODO: Move this and the duplication in `RedisDatabase` to a shared utility class. 
+        private static void EvaluateResultAndThrow(ParseResult result, [CallerMemberName] string callingMember = "")
+        {
+            if (result.Type == ResultType.Error)
+            {
+                throw new RedisServerException($"Redis returned an error while we were invoking `{callingMember}`. The error was: {result.ToString()}");
+            }
         }
     }
 }
