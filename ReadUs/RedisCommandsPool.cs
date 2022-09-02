81973fe67b5f9b32fd62fe94dac4208e9517eca8
Tommy Hanks <tommy.hanks@gmail.com>
2022-09-02 18:26:07 -0400

    Started to create the proper abstractions so that I can hide some of the details of the underlying Redis connection away from the user.

--- ReadUs/RedisCommandsPool.cs
+++ ReadUs/RedisCommandsPool.cs
@@ -4,13 +4,15 @@ using System.Threading.Tasks;
 
 namespace ReadUs
 {
-    public class RedisCommandsPool : IRedisConnectionPool
+    // Let's make RedisCommandsPool an abstract class and essentially move all of this stuff into a
+    // new class called RedisSingleInstanceCommandsPool. 
+    // 
+    // We'll 
+
+    public abstract class RedisCommandsPool : IRedisConnectionPool
     {
         protected readonly string _serverAddress;
         protected readonly int _serverPort;
-
-        private readonly ConcurrentQueue<IRedisConnection> _backingPool = new ConcurrentQueue<IRedisConnection>();
-        private readonly List<IRedisConnection> _allConnections = new List<IRedisConnection>();
 
         public RedisCommandsPool(string serverAddress, int serverPort)
         {
@@ -18,41 +20,10 @@ namespace ReadUs
             _serverPort = serverPort;
         }
 
-        public async Task<IRedisDatabase> GetAsync()
-        {
-            var connection = GetReadUsConnection();
+        public abstract Task<IRedisDatabase> GetAsync();
 
-            if (!connection.IsConnected)
-            {
-                await connection.ConnectAsync();    
-            }
+        public abstract void ReturnConnection(IRedisConnection connection);
 
-            return new RedisSingleInstanceDatabase(connection, this);
-        }
-
-        private IRedisConnection GetReadUsConnection()
-        {
-            if (_backingPool.TryDequeue(out var connection))
-            {
-                return connection;
-            }
-
-            var newConnection = new RedisConnection(_serverAddress, _serverPort);
-
-            _allConnections.Add(newConnection);
-
-            return newConnection;
-        }
-
-        public void ReturnConnection(IRedisConnection connection) =>
-            _backingPool.Enqueue(connection);
-
-        public void Dispose()
-        {
-            foreach (var connection in _allConnections)
-            {
-                connection.Dispose();
-            }
-        }
+        public abstract void Dispose();
     }
 }
---
+++ ReadUs/RedisSingleInstanceCommandsPool.cs
@@ -0,0 +1,53 @@
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ReadUs
+{
+    public class RedisSingleInstanceCommandsPool : RedisCommandsPool
+    {
+        private readonly ConcurrentQueue<IRedisConnection> _backingPool = new ConcurrentQueue<IRedisConnection>();
+        private readonly List<IRedisConnection> _allConnections = new List<IRedisConnection>();
+
+        public RedisSingleInstanceCommandsPool(string serverAddress, int serverPort) :
+            base(serverAddress, serverPort)
+        { }
+
+        public override async Task<IRedisDatabase> GetAsync()
+        {
+            var connection = GetReadUsConnection();
+
+            if (!connection.IsConnected)
+            {
+                await connection.ConnectAsync();
+            }
+
+            return new RedisSingleInstanceDatabase(connection, this);
+        }
+
+        private IRedisConnection GetReadUsConnection()
+        {
+            if (_backingPool.TryDequeue(out var connection))
+            {
+                return connection;
+            }
+
+            var newConnection = new RedisConnection(_serverAddress, _serverPort);
+
+            _allConnections.Add(newConnection);
+
+            return newConnection;
+        }
+
+        public override void ReturnConnection(IRedisConnection connection) =>
+            _backingPool.Enqueue(connection);
+
+        public override void Dispose()
+        {
+            foreach (var connection in _allConnections)
+            {
+                connection.Dispose();
+            }
+        }
+    }
+}
