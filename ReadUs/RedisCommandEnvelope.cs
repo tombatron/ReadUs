7f0254fae81e31050dc6e0c855910ec6ff35d299
Tommy Hanks <tommy.hanks@gmail.com>
2022-09-10 08:45:47 -0400

    Cleaned up the unused import there.

--- ReadUs/RedisCommandEnvelope.cs
+++ ReadUs/RedisCommandEnvelope.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using static ReadUs.Encoder.Encoder;
 using static ReadUs.ParameterUtilities;
 using static ReadUs.RedisCommandNames;
