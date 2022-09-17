f17b81ad4733933194ec6c38ba08f2ed5ad6a3f6
Tommy Hanks <tommy.hanks@gmail.com>
2022-09-17 09:55:23 -0400

    Fixed nullability analysis issue here.

--- ReadUs/ResultModels/ClusterNodeAddress.cs
+++ ReadUs/ResultModels/ClusterNodeAddress.cs
@@ -5,16 +5,7 @@ namespace ReadUs.ResultModels
 {
     public class ClusterNodeAddress
     {
-        private ClusterNodeAddress(char[] rawValue) =>
-            InitializeValues(rawValue);
-        
-        public IPAddress IpAddress { get; private set; }
-        
-        public int RedisPort { get; private set; }
-        
-        public int ClusterPort { get; private set; }
-
-        private void InitializeValues(char[] rawValue)
+        private ClusterNodeAddress(char[] rawValue)
         {
             var startIndex = 0;
             var nextDelimiter = Array.IndexOf(rawValue, ':', startIndex);
@@ -30,6 +21,12 @@ namespace ReadUs.ResultModels
 
             ClusterPort = int.Parse(rawValue[startIndex..]);
         }
+
+        public IPAddress IpAddress { get; private set; }
+
+        public int RedisPort { get; private set; }
+
+        public int ClusterPort { get; private set; }
 
         public static implicit operator ClusterNodeAddress(char[] rawValue) =>
             new ClusterNodeAddress(rawValue);
