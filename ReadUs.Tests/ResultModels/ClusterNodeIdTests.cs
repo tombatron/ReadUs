f8ef7335c6d8e076f2130bff46145fa854e983af
Tommy Hanks <tommy.hanks@gmail.com>
2022-09-18 09:09:21 -0400

    Added in some covering tests for ClusterNodeId.

---
+++ ReadUs.Tests/ResultModels/ClusterNodeIdTests.cs
@@ -0,0 +1,31 @@
+using ReadUs.ResultModels;
+using Xunit;
+
+namespace ReadUs.Tests.ResultModels
+{
+    public class ClusterNodeIdTests
+    {
+        public class ItCanImplicitlyCastTo
+        {
+            [Fact]
+            public void ClusterNodeIdFromCharArray()
+            {
+                var expectedClusterNodeId = new ClusterNodeId("Hello World".ToCharArray());
+
+                var castClusterNodeId = (ClusterNodeId)"Hello World".ToCharArray();
+
+                Assert.Equal(expectedClusterNodeId, castClusterNodeId);
+            }
+
+            [Fact]
+            public void StringFromClusterNodeId()
+            {
+                var testClusterNodeId = new ClusterNodeId("Good night".ToCharArray());
+
+                var stringClusterNodeId = (string)testClusterNodeId;
+
+                Assert.Equal("Good night", stringClusterNodeId);
+            }
+        }
+    }
+}
--- ReadUs/ResultModels/ClusterNodeId.cs
+++ ReadUs/ResultModels/ClusterNodeId.cs
@@ -1,10 +1,13 @@
-﻿namespace ReadUs.ResultModels
-{
-    public class ClusterNodeId
-    {
-        private readonly char[] _rawValue;
+﻿using System;
+using System.Linq;
 
-        private ClusterNodeId(char[] rawValue) =>
+namespace ReadUs.ResultModels
+{
+    public sealed class ClusterNodeId
+    {
+        internal readonly char[] _rawValue;
+
+        internal ClusterNodeId(char[] rawValue) =>
             _rawValue = rawValue;
 
         public static implicit operator ClusterNodeId(char[] rawValue) =>
@@ -12,5 +15,32 @@
 
         public static implicit operator string(ClusterNodeId nodeId) =>
             new string(nodeId._rawValue);
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is ClusterNodeId otherClusterNodeId)
+            {
+                return GetHashCode() == otherClusterNodeId.GetHashCode();
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 13;
+
+                foreach(var c in _rawValue)
+                {
+                    hash += (hash * 7) + c;
+                }
+
+                return hash;
+            }
+        }
     }
 }
