dda4286aaa824814e1463f870b579029ea6ad2fe
Tommy Hanks <tommy.hanks@gmail.com>
2025-01-30 17:11:29 -0500

    Fixed Redis single instance database access.

--- ReadUs/RedisSingleInstanceDatabase.cs
+++ ReadUs/RedisSingleInstanceDatabase.cs
@@ -1,69 +1,84 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using ReadUs.Parser;
 using ReadUs.ResultModels;
 using static ReadUs.Parser.Parser;
 
 namespace ReadUs;
 
-public class RedisSingleInstanceDatabase : RedisDatabase
+public class RedisSingleInstanceDatabase(IRedisConnection connection, RedisConnectionPool pool) : RedisDatabase(connection, pool)
 {
-    public RedisSingleInstanceDatabase(IRedisConnection connection, RedisConnectionPool pool) : base(connection, pool)
-    {
-    }
+    public IRedisConnection UnderlyingConnection => connection;
 
-    public IRedisConnection UnderlyingConnection => _connection;
-
-    public override async Task SelectAsync(int databaseId, CancellationToken cancellationToken = default)
+    public override async Task<Result> SelectAsync(int databaseId, CancellationToken cancellationToken = default)
     {
-        IsDisposed();
+        if (IsDisposed(out var error))
+        {
+            return Result.Error(error!);
+        }
 
         var command = RedisCommandEnvelope.CreateSelectCommand(databaseId);
 
-        var rawResult = await _connection.SendCommandAsync(command, cancellationToken).ConfigureAwait(false);
+        var rawResult = await Connection.SendCommandAsync(command, cancellationToken).ConfigureAwait(false);
 
-        var result = Parse(rawResult);
+        var result = Parse(rawResult) switch
+        {
+            Ok<ParseResult> => Result.Ok,
+            Error<ParseResult> err => Result.Error(err.Message),
+            _ => Result.Error("An unexpected error occurred while attempting to parse the result of the SELECT command.")
+        };
 
-        EvaluateResult(result);
+        return result;
     }
 
-    public override Task<BlockingPopResult> BlockingLeftPopAsync(params RedisKey[] keys)
+    public override Task<Result<BlockingPopResult>> BlockingLeftPopAsync(params RedisKey[] keys) =>
+        BlockingLeftPopAsync(TimeSpan.MaxValue, keys);
+    
+    public override async Task<Result<BlockingPopResult>> BlockingLeftPopAsync(TimeSpan timeout, params RedisKey[] keys)
     {
-        return BlockingLeftPopAsync(TimeSpan.MaxValue, keys);
-    }
-
-    public override async Task<BlockingPopResult> BlockingLeftPopAsync(TimeSpan timeout, params RedisKey[] keys)
-    {
-        IsDisposed();
+        if (IsDisposed(out var error))
+        {
+            return Result<BlockingPopResult>.Error(error!);
+        }
 
         var command = RedisCommandEnvelope.CreateBlockingLeftPopCommand(keys, timeout);
 
-        var rawResult = await _connection.SendCommandAsync(command).ConfigureAwait(false);
+        var rawResult = await Connection.SendCommandAsync(command).ConfigureAwait(false);
 
-        var result = Parse(rawResult);
+        var result = Parse(rawResult) switch
+        {
+            Ok<ParseResult> ok => EvaluateResult<BlockingPopResult>(ok.Value, (pr) => Result<BlockingPopResult>.Ok((BlockingPopResult)pr)),
+            Error<ParseResult> err => Result<BlockingPopResult>.Error(err.Message),
+            _ => Result<BlockingPopResult>.Error("An unexpected error occurred while attempting to parse the result of the BLPOP command.")
+        };
 
-        EvaluateResult(result);
-
-        return (BlockingPopResult)result;
+        return result;
     }
 
-    public override Task<BlockingPopResult> BlockingRightPopAsync(params RedisKey[] keys)
+    public override Task<Result<BlockingPopResult>> BlockingRightPopAsync(params RedisKey[] keys)
     {
         return BlockingRightPopAsync(TimeSpan.MaxValue, keys);
     }
 
-    public override async Task<BlockingPopResult> BlockingRightPopAsync(TimeSpan timeout, params RedisKey[] keys)
+    public override async Task<Result<BlockingPopResult>> BlockingRightPopAsync(TimeSpan timeout, params RedisKey[] keys)
     {
-        IsDisposed();
+        if (IsDisposed(out var error))
+        {
+            return Result<BlockingPopResult>.Error(error!);
+        }
 
         var command = RedisCommandEnvelope.CreateBlockingRightPopCommand(keys, timeout);
 
-        var rawResult = await _connection.SendCommandAsync(command).ConfigureAwait(false);
+        var rawResult = await Connection.SendCommandAsync(command).ConfigureAwait(false);
 
-        var result = Parse(rawResult);
+        var result = Parse(rawResult) switch
+        {
+            Ok<ParseResult> ok => EvaluateResult<BlockingPopResult>(ok.Value, (pr) => Result<BlockingPopResult>.Ok((BlockingPopResult)pr)),
+            Error<ParseResult> err => Result<BlockingPopResult>.Error(err.Message),
+            _ => Result<BlockingPopResult>.Error("An unexpected error occurred while attempting to parse the result of the BRPOP command.")
+        };
 
-        EvaluateResult(result);
-
-        return (BlockingPopResult)result;
+        return result;
     }
 }
