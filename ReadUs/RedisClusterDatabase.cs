5f5e37f3ed401da36f975837200ea024eead5117
Tommy Hanks <tommy.hanks@gmail.com>
2025-01-30 17:04:24 -0500

    Updated to use the results pattern like everything else.

--- ReadUs/RedisClusterDatabase.cs
+++ ReadUs/RedisClusterDatabase.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading;
 using System.Threading.Tasks;
+using ReadUs.Parser;
 using ReadUs.ResultModels;
 using static ReadUs.Parser.Parser;
 
@@ -14,61 +16,78 @@ public class RedisClusterDatabase : RedisDatabase
     {
     }
 
-    public override Task<BlockingPopResult> BlockingLeftPopAsync(params RedisKey[] keys)
+    public override Task<Result<BlockingPopResult>> BlockingLeftPopAsync(params RedisKey[] keys)
     {
         return BlockingLeftPopAsync(TimeSpan.MaxValue, keys);
     }
 
-    public override async Task<BlockingPopResult> BlockingLeftPopAsync(TimeSpan timeout, params RedisKey[] keys)
+    public override async Task<Result<BlockingPopResult>> BlockingLeftPopAsync(TimeSpan timeout, params RedisKey[] keys)
     {
-        IsDisposed();
+        if(IsDisposed(out var error))
+        {
+            return Result<BlockingPopResult>.Error(error!);
+        }
 
         var command = RedisCommandEnvelope.CreateBlockingLeftPopCommand(keys, timeout);
 
-        var rawResult = await _connection.SendCommandAsync(command).ConfigureAwait(false);
+        var rawResult = await Connection.SendCommandAsync(command).ConfigureAwait(false);
 
-        var result = Parse(rawResult);
+        var result = Parse(rawResult) switch
+        {
+            Ok<ParseResult> ok => EvaluateResult<BlockingPopResult>(ok.Value, (pr) => Result<BlockingPopResult>.Ok((BlockingPopResult)pr)),
+            Error<ParseResult> err => Result<BlockingPopResult>.Error(err.Message),
+            _ => Result<BlockingPopResult>.Error("An unexpected error occurred while attempting to parse the result of the BLPOP command.")
+        };
 
-        EvaluateResult(result);
-
-        return (BlockingPopResult)result;
+        return result;
     }
 
-    public override Task<BlockingPopResult> BlockingRightPopAsync(params RedisKey[] keys)
-    {
-        return BlockingRightPopAsync(TimeSpan.MaxValue, keys);
-    }
+    public override Task<Result<BlockingPopResult>> BlockingRightPopAsync(params RedisKey[] keys) => 
+        BlockingRightPopAsync(TimeSpan.MaxValue, keys);
 
-    public override async Task<BlockingPopResult> BlockingRightPopAsync(TimeSpan timeout, params RedisKey[] keys)
+    public override async Task<Result<BlockingPopResult>> BlockingRightPopAsync(TimeSpan timeout, params RedisKey[] keys)
     {
-        IsDisposed();
-
+        if(IsDisposed(out var error))
+        {
+            return Result<BlockingPopResult>.Error(error!);
+        }
+        
         var command = RedisCommandEnvelope.CreateBlockingRightPopCommand(keys, timeout);
 
-        var rawResult = await _connection.SendCommandAsync(command).ConfigureAwait(false);
+        var rawResult = await Connection.SendCommandAsync(command).ConfigureAwait(false);
 
-        var result = Parse(rawResult);
+        var result = Parse(rawResult) switch
+        {
+            Ok<ParseResult> ok => EvaluateResult<BlockingPopResult>(ok.Value, (pr) => Result<BlockingPopResult>.Ok((BlockingPopResult)pr)),
+            Error<ParseResult> err => Result<BlockingPopResult>.Error(err.Message),
+            _ => Result<BlockingPopResult>.Error("An unexpected error occurred while attempting to parse the result of the BRPOP command.")
+        };
 
-        EvaluateResult(result);
-
-        return (BlockingPopResult)result;
+        return result;
     }
 
-    public override Task SelectAsync(int databaseId, CancellationToken cancellationToken = default)
+    public override Task<Result> SelectAsync(int databaseId, CancellationToken cancellationToken = default)
     {
-        IsDisposed();
+        if (IsDisposed(out var error))
+        {
+            return Task.FromResult(Result.Error(error!));
+        }
 
         // No-Op, this command doesn't really do anything on clusters. 
 
-        return Task.CompletedTask;
+        return Task.FromResult(Result.Ok);
     }
 
-    public override async Task SetMultipleAsync(KeyValuePair<RedisKey, string>[] keysAndValues,
+    public override async Task<Result> SetMultipleAsync(KeyValuePair<RedisKey, string>[] keysAndValues,
         CancellationToken cancellationToken = default)
     {
         var keyGroups = keysAndValues.GroupBy(x => x.Key.Slot);
 
-        var setMultipleTasks = new List<Task>();
+        // TODO: I don't like this. I think the default behavior should be to return an error if the keys
+        //       don't belong to the same slot. Should we support setting keys on multiple slots? Sure I guess, 
+        //       but if we do that, I think we could be a bit smarter about it than what we're doing here. 
+        
+        var setMultipleTasks = new List<Task<Result>>();
 
         foreach (var keyGroup in keyGroups)
         {
@@ -76,5 +95,23 @@ public class RedisClusterDatabase : RedisDatabase
         }
 
         await Task.WhenAll(setMultipleTasks);
+        
+        // I don't like this...
+        foreach (var setTask in setMultipleTasks)
+        {
+            var setResult = await setTask;
+
+            if (setResult is Ok)
+            {
+                // No - Op, dumb.
+            }
+            
+            if (setResult is Error err)
+            {
+                return err;
+            }
+        }
+        
+        return Result.Ok;
     }
 }
