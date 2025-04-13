df36ffc2c4496872ab570ff1d12f0ed97ba196e1
Tommy Hanks <tommy.hanks@gmail.com>
2025-04-13 09:11:01 -0400

    Initial support for SUBSCRIBE, UNSUBSCRIBE, and PUBLISH complete.

--- ReadUs.Tests/RedisSubscriptionTests.cs
+++ ReadUs.Tests/RedisSubscriptionTests.cs
@@ -9,7 +9,8 @@ public sealed class RedisSubscriptionTests(RedisSingleInstanceFixture fixture) :
     [Fact]
     public async Task ItCanSubscribeAndReceiveMessages()
     {
-        var pool = RedisConnectionPool.Create(fixture.GetConnectionString());
+        // var pool = RedisConnectionPool.Create(fixture.GetConnectionString());
+        var pool = RedisConnectionPool.Create(new Uri("redis://tombaserver.local:6379"));
 
         var db = await pool.GetAsync();
 
@@ -20,11 +21,11 @@ public sealed class RedisSubscriptionTests(RedisSingleInstanceFixture fixture) :
             subscriptionMessage = message;
         });
         
-        await Task.Delay(TimeSpan.FromMilliseconds(100));
+        await Task.Delay(TimeSpan.FromMilliseconds(1));
         
         await db.Publish("channel", "hello world");
         
-        await Task.Delay(TimeSpan.FromMilliseconds(100));
+        await Task.Delay(TimeSpan.FromMilliseconds(1));
         
         Assert.Equal("hello world", subscriptionMessage);
     }
@@ -32,7 +33,8 @@ public sealed class RedisSubscriptionTests(RedisSingleInstanceFixture fixture) :
     [Fact]
     public async Task ItCanSubscribeAndUnsubscribe()
     {
-        var pool = RedisConnectionPool.Create(fixture.GetConnectionString());
+        //var pool = RedisConnectionPool.Create(fixture.GetConnectionString());
+        var pool = RedisConnectionPool.Create(new Uri("redis://tombaserver.local:6379"));
         
         var db = await pool.GetAsync();
         
@@ -52,7 +54,7 @@ public sealed class RedisSubscriptionTests(RedisSingleInstanceFixture fixture) :
             }
         });
         
-        await Task.Delay(TimeSpan.FromMilliseconds(10));
+        await Task.Delay(TimeSpan.FromMilliseconds(1));
         
         await db.Publish("channel_1", "hello world");
         await db.Publish("channel_2", "hello world");
@@ -61,6 +63,10 @@ public sealed class RedisSubscriptionTests(RedisSingleInstanceFixture fixture) :
         
         await db.Publish("channel_1", "goodnight moon");
         await db.Publish("channel_2", "goodnight moon");
+        
+        // TODO: Keep an eye on this... On one hand this is a code smell. On the other hand, we are sending a message to a server
+        //       even if it's hosted locally in Docker it still needs a moment to be received and propagated. 
+        await Task.Delay(TimeSpan.FromMilliseconds(1));
         
         Assert.Equal("hello world", channel1Message);
         Assert.Equal("goodnight moon", channel2Message);
