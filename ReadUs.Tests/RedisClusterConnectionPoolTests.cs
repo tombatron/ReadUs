d6339244900a1171cc11d0e64bd3ffa36b416408
Tom Hanks <tommy.hanks@gmail.com>
2025-05-03 09:35:43 -0400

    Fixed tests.
    We were still calling `GetAsync` with the `using` statement. That didn't
    make sense anymore.
    
    At the suggestion of Rider I also converted the constructor to a primary
    constructor, I think this looks a lot cleaner, so it stays!

--- ReadUs.Tests/RedisClusterConnectionPoolTests.cs
+++ ReadUs.Tests/RedisClusterConnectionPoolTests.cs
@@ -4,22 +4,15 @@ using Xunit;
 namespace ReadUs.Tests;
 
 [Collection(nameof(RedisClusterFixtureCollection))]
-public class RedisClusterConnectionPoolTests
+public class RedisClusterConnectionPoolTests(RedisClusterFixture redisClusterFixture)
 {
-    private readonly RedisClusterFixture _redisClusterFixture;
-
-    public RedisClusterConnectionPoolTests(RedisClusterFixture redisClusterFixture)
-    {
-        _redisClusterFixture = redisClusterFixture;
-    }
-
     [Fact]
     public async Task ItCanGetDatabaseInstance()
     {
         using var clusterConnectionPool = new RedisClusterConnectionPool(
-            _redisClusterFixture.ClusterNodes, _redisClusterFixture.Configuration);
+            redisClusterFixture.ClusterNodes, redisClusterFixture.Configuration);
 
-        using var redisDatabase = await clusterConnectionPool.GetAsync();
+        var redisDatabase = await clusterConnectionPool.GetAsync();
 
         Assert.IsType<RedisClusterDatabase>(redisDatabase);
     }
