b8378c6cc883134140e45f2cb4d5f07a4e684638
Tom Hanks <tommy.hanks@gmail.com>
2025-10-25 16:53:13 -0400

    Fixed tests here.

--- ReadUs.Tests/RedisConnectionConfigurationTests.cs
+++ ReadUs.Tests/RedisConnectionConfigurationTests.cs
@@ -11,13 +11,13 @@ public class RedisConnectionConfigurationTests
     [Fact]
     public void ItCanImplicitlyConvertFromUri()
     {
-        var connectionUri = new Uri("redis://localhost:6379?connectionsPerNode=10");
+        var connectionUri = new Uri("redis://localhost:6379?connectionName=Tombatron");
 
         RedisConnectionConfiguration configuration = connectionUri;
 
         Assert.Equal("localhost", configuration.ServerAddress);
         Assert.Equal(6379, configuration.ServerPort);
-        Assert.Equal(10, configuration.ConnectionsPerNode);
+        Assert.Equal("Tombatron", configuration.ConnectionName);
     }
 
     [Fact]
@@ -31,18 +31,5 @@ public class RedisConnectionConfigurationTests
         });
 
         Assert.Equal("The provided scheme `http` is invalid, it must be `redis`.", exception.Message);
-    }
-
-    [Fact]
-    public void ItWillThrowAnExceptionIfConnectionsPerNodeIsntAValidInteger()
-    {
-        var connectionUri = new Uri("redis://localhost?connectionsPerNode=whatever");
-
-        var exception = Assert.Throws<RedisConnectionConfigurationException>(() =>
-        {
-            RedisConnectionConfiguration configuration = connectionUri;
-        });
-
-        Assert.Equal("`connectionsPerNode` must be a valid integer.", exception.Message);
     }
 }
